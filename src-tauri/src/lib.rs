4d16415dffd475933f301eceaaedb6e0c25123a0
nounfve <rootastgrfvbgh564778hj@gmail.com>
2026-09-12 13:58:11 +0800

    fix(deeplink): (propose)fix deep link not handled on cold start and double handled when trigger by singleton plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -655,7 +655,8 @@ pub fn run() {
         .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
             for arg in args {
                 if arg.starts_with("flobro://") {
-                    handle_deep_link(app, &arg);
+                    // if handle deep link then not bring launcher on front
+                    return;
                 }
             }
             if let Some(launcher) = app.get_webview_window("launcher") {
@@ -781,8 +782,15 @@ pub fn run() {
                 });
             }
 
-            // deep links delivered while running (macOS) or on cold start
             let dl_handle = handle.clone();
+            // app was likely started by a deep link
+            if let Some(urls) = app.deep_link().get_current()? {
+                for url in urls {
+                    handle_deep_link(&dl_handle, url.as_str());
+                }
+            };
+
+            // deep links delivered while running (macOS) or on cold start
             app.deep_link().on_open_url(move |event| {
                 for url in event.urls() {
                     handle_deep_link(&dl_handle, url.as_str());
